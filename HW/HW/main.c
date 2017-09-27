30f3f3ea78d22f8d554ffed99cec044aa611e137
陳匯禮 <tp6cjo4xu3@gmail.com>
2017-09-28 02:22:46 +0800

    HW1_2.25

---
+++ HW/HW/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("EEEEEEEEEEEEEEE\n");
+	printf("E      E      E\n");
+	printf("E      E      E\n");
+	printf("E      E      E\n");
+	printf("E      E      E\n");
+	printf("               \n");
+	printf("DDDDDDDDDDDDDDD\n");
+	printf("D             D\n");
+	printf(" D           D \n");
+	printf("  D         D  \n");
+	printf("    DDDDDDDD   \n");
+	system("pause");
+	return 0;
+}
